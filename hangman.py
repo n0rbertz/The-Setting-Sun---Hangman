7df6711c1eb6173e97afbb02b2e9f8b0ca929d08
ellisman31 <akos99@protonmail.com>
2020-11-30 14:15:46 +0100

    new hangman

--- hangman.py
+++ hangman.py
@@ -1,5 +1,46 @@
-def play(word, lives):
-    pass
+import random #for the random order
 
+name = input("Enter your name: ")
+print("Good Luck", name+ "!")
+words = [
+    "hangman",
+    "computer",
+    "science",
+    "programming",
+    "python",
+    "television"
+    "mathematics",
+    "player",
+    "telephone",
+    "visual studio",
+    "milk",
+    "apple",
+    "water",
+    "tomato",
+    "potato",
+    "CodeCool"
+]
+# Function will choose one random
+# word from this list of words
+word = random.choice(words)
+print("\nHangman Singeplayer\nGuess the word")
+guesses = ""
+# any number of turns can be used here
+lives = 12
+while lives > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end="")
+        else:
+            print("_", end="")
+            failed += 1
+    if failed == 0:
+        print("\n\nYou Win!")
+        print("\nThe word is:", word)
+        break
 
-play('Codecool', 6)
+    guess = input("\n\nGuess the character: ")
+    guesses += guess
+    if guess not in word: #Not in the list
+        lives -= 1 #-1 
