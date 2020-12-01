717854632de2a52d3a51bb2bf3d925f6cc82eb0b
ellisman31 <akos99@protonmail.com>
2020-12-01 13:23:24 +0100

    new

--- practice.py
+++ practice.py
@@ -4,17 +4,33 @@ import sys
 import time
 import os
 
+def clear(): os.system('clear')
+reminder = pyfiglet.figlet_format("Friendly Reminder")
+print(reminder+"\nIf you want to [Quit], just type exit.\n")
+print("Loading")
+time.sleep(2)
+print(".")
+time.sleep(1)
+print(".")
+time.sleep(1)
+clear()
 banner = pyfiglet.figlet_format("Hangman")
 print(banner)
 
 name = input("Before we start, please type your name: ")
-menu = input("Welcome {0}!\nDo you want to play? ".format(name))
+cap = name.capitalize()
+clear()
+
+menuset = input("Welcome {0}!\nDo you want to play? ".format(cap))
 no = pyfiglet.figlet_format("See you next time!")
 
-if menu == "yes":
+if menuset == "yes":
     print("Alright, let's play!")
-elif menu == "no":
-    print(no)
+    time.sleep(1)
+    clear()
+elif menuset == "no":
+    clear()
+    print(no) 
     time.sleep(1)
     print(".")
     time.sleep(1)
@@ -23,48 +39,40 @@ elif menu == "no":
     print(".")
     sys.exit()
 else:
-    print("Command is unavaliable")
+     print("Command is unavaliable")
 
-levels = input("Please select a level (easy, normal, hard, expert): ")
-    if levels == "easy":
 
-    elif levels == "normal":
+levels = input("Please select a level (easy, normal, hard): ")
+ #   if levels == "easy": #pár betű a szóból
+
+  #  elif levels == "normal":
     
     elif levels == "hard":
 
-    elif levels == "expert":
-        
+  # else:
+   # print("You typed something wrong! Try again.")
+
+print("Your chose level is: {0}.\n".format(levels))
+
 goodluck = pyfiglet.figlet_format("Good Luck!\n")
 print(goodluck)
 
+time.sleep(1)
+clear()
 
-words = [
-    "hangman",
-    "computer",
-    "science",
-    "programming",
-    "python",
-    "television"
-    "mathematics",
-    "player",
-    "telephone",
-    "visual studio",
-    "milk",
-    "apple",
-    "water",
-    "tomato",
-    "potato",
-    "CodeCool"
-]
+words = open("countries-and-capitals.txt","r")  #import txt file
+
 word = random.choice(words)
 guesses = ""
-# any number of turns can be used here
+#life = pyfiglet.figlet_format("Lives: ")
+
 lives = 12
 while lives > 0:
+    print("Lives: " + str(lives))
     failed = 0
     for char in word:
         if char in guesses:
-            print(char, end="")
+            print(char + " ", end="")
         else:
             print("_ ", end="")
             failed += 1
@@ -74,6 +82,10 @@ while lives > 0:
         break
 
     guess = input("\n\nGuess the character: ")
-    guesses += guess
-    if guess not in word: #Not in the list
-        lives -= 1 #-1 
+    if guess in guesses:
+        print("You already used this letter")
+    if guess not in word and guess not in guesses:
+        lives -= 1 #-1
+    if guess not in guesses:
+        guesses += guess
+    print("Guessed letters: " + guesses +",")
